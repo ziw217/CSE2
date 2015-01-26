30e2f731b7d5bf467cd2382b5eb383cec7764510
ziw217 <ziw217@lehigh.edu>
2015-01-26 17:12:48 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////
+//Zijia Wang
+//Demo 0
+// Welcome Class Java Program
+//
+// first compile the program
+       //javac WelcomeClass.java
+// run the program
+//     java WelcomeClass//
+
+// define a class
+ public class WelcomeClass{
+ 
+// add main method
+ public static void main(String[]args){
+     
+// print the stattement with box around it
+   System.out.println("----------------------");
+   System.out.println("  |  WELCOME  |");
+   System.out.println("----------------------");
+   System.out.println("  ^  ^  ^  ^  ^  ^");
+   System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+   System.out.println("<-Z--I--W--2--1--7->");
+   System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+   System.out.println("  v  v  v  v  v  v");
+   
+   //test/testtest
+  }
+ }
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Brian Chen
+//Demo 0
+//Hello World Jave Program
+//
+// first compile the program
+//     javac HelloWorld.java
+// run the program
+//     jave HelloWorld//
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+  public static void main(String[] args) {
+      
+// print the stattement with box around it
+   System.out.println("#######################");
+   System.out.println("#######################");
+   System.out.println("#                     #");
+   System.out.println("#  Hello World 2014!! #");
+   System.out.println("#                     #");
+   System.out.println("#######################");
+   System.out.println("#######################");
+   System.out.println("test");
+   
+   //test/testtest
+  }
+}
